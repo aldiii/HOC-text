7b07eebace8450243be2034dc5700c9a1e9f05d3
aldiii <a.tomzik@gmail.com>
2021-03-05 12:31:46 +0100

    add WithOnOffState function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { CheckBoxWithState } from "./CheckBoxWithState";
-import { RadioGroupWithState } from "./RadioGroupWithState";
+import { CheckBox } from "./CheckBox";
+import { RadioGroup } from "./RadioGroup";
 
 import "./styles.css";
+
+function WithOnOffState(StatelessComponent) {
+  function EnhancedComponent({ children }) {
+    const [on, setOn] = useState(false);
+    const toggle = () => setOn((prevOn) => !prevOn);
+    return (
+      <StatelessComponent on={on} toggle={toggle}>
+        {children}
+      </StatelessComponent>
+    );
+  }
+  return EnhancedComponent;
+}
+
+const CheckBoxWithState = WithOnOffState(CheckBox);
+const RadioGroupWithState = WithOnOffState(RadioGroup);
 
 function App() {
   return (
